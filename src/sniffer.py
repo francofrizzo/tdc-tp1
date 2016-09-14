111b61a7738e62950e3c952eb101d61fce54184e
Iván Pondal <ivan.pondal@gmail.com>
2016-09-14 16:42:10 -0300

    Add entropy and symbol information for source S

--- src/sniffer.py
+++ src/sniffer.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2
 import sys
+from math import log
 from scapy.all import Ether, Dot11, sniff
 
 BROADCAST_PHYSICAL_ADDRESS = 'ff:ff:ff:ff:ff:ff'
@@ -22,4 +23,17 @@ broadcast_packets = len([1 for pkt in sniffed_packets \
     if (Ether in pkt and pkt.dst == BROADCAST_PHYSICAL_ADDRESS) or \
        (Dot11 in pkt and pkt.addr1 == BROADCAST_PHYSICAL_ADDRESS)])
 
-print broadcast_packets * 1.0 / len(sniffed_packets)
+broadcast_p = broadcast_packets * 1.0 / len(sniffed_packets)
+not_broadcast_p = 1 - broadcast_p
+broadcast_i = -log(broadcast_p, 2)
+not_broadcast_i = -log(not_broadcast_p, 2)
+s_entropy = broadcast_p*broadcast_i + not_broadcast_p*not_broadcast_i
+
+print '\nFuente S'
+print '--------'
+print 'E = {Paquete con destino broadcast}'
+print 'P(E) = ' + str(broadcast_p)
+print 'P(!E) = ' + str(not_broadcast_p)
+print 'I(E) = ' + str(broadcast_i) + ' bits'
+print 'I(!E) = ' + str(not_broadcast_i) + ' bits'
+print 'H(S) = ' + str(s_entropy) + ' bits'
